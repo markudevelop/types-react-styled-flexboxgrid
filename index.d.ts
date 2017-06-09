ba8bdd3e4eb6bd09d9bacba2e4aa36541e666d2a
Mark Uretsky <zainaf24@gmail.com>
2017-06-09 17:11:31 +0200

    Create index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,38 @@
-type Sizes = 'xs' | 'sm' | 'md' | 'lg';
-
-interface RowProps {
-  around?: Sizes;
-  between?: Sizes;
-  bottom?: Sizes;
-  center?: Sizes;
-  end?: Sizes;
-  first?: Sizes;
-  last?: Sizes;
-  middle?: Sizes;
-  reverse?: boolean;
-  start?: Sizes;
-  style?: {};
-  top?: Sizes;
-}
-
-interface GridProps {
-  fluid?: boolean;
-  style?: {};
-}
-
-interface ColProps {
-  lg?: boolean | number;
-  lgOffset?: number;
-  md?: boolean | number;
-  mdOffset?: number;
-  reverse?: boolean;
-  sm?: boolean | number;
-  smOffset?: number;
-  xs?: boolean | number;
-  xsOffset?: number;
-}
-
 declare module 'react-styled-flexboxgrid' {
+  type Sizes = 'xs' | 'sm' | 'md' | 'lg';
+
+  interface RowProps {
+    around?: Sizes;
+    between?: Sizes;
+    bottom?: Sizes;
+    center?: Sizes;
+    end?: Sizes;
+    first?: Sizes;
+    last?: Sizes;
+    middle?: Sizes;
+    reverse?: boolean;
+    start?: Sizes;
+    style?: {};
+    top?: Sizes;
+  }
+
+  interface GridProps {
+    fluid?: boolean;
+    style?: {};
+  }
+
+  interface ColProps {
+    lg?: boolean | number;
+    lgOffset?: number;
+    md?: boolean | number;
+    mdOffset?: number;
+    reverse?: boolean;
+    sm?: boolean | number;
+    smOffset?: number;
+    xs?: boolean | number;
+    xsOffset?: number;
+  }
+                
   export class Grid extends React.Component<GridProps, any> {}
   export class Row extends React.Component<RowProps, any> {}
   export class Col extends React.Component<ColProps, any> {}
